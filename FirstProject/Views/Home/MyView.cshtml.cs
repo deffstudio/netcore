0248c9db857018a0ae93d67e9d7320a79bb9d257
deffstudio <dudeyahuu@gmail.com>
2024-03-23 06:27:36 +0700

    Added First ASP.NET Core MVC project

---
+++ FirstProject/Views/Home/MyView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class MyViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
